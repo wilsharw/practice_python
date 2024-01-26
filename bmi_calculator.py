d15e2e0c2b83ded6f1589361264ed8d845ba3b8e
Wilsha RW <wilsharw@gmail.com>
2024-01-27 00:30:16 +0100

    Writing calculated BMI to file

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,3 +1,5 @@
+name= input("Enter your name\n")
+age= input("Enter your age\n")
 weight=float(input("Enter your weight in kilograms\n"))
 height=float(input("Enter your height in meters\n"))
 
@@ -9,3 +11,16 @@ elif BMI >25:
    print("overweight")
 elif BMI >18.5 and BMI <25:
    print("Normal")
+
+f = open("bmi_calculator.csv", "a+")
+f2 = open("bmi_calculator.csv", "r+")
+
+file_content = f2.read()
+
+headings = "Name,Age,Weight,Height,BMI\n"
+if headings not in file_content:
+   f.write(headings)
+
+f.write(f"{name},{age},{weight},{height},{BMI}\n")
+f.close()
+f2.close()
